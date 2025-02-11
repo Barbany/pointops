f2ae64cb423f35e76526d67deadd3271f0a58e48
Barbany <oriolbm96@gmail.com>
2025-02-11 11:23:26 +0000

    search subdirectories for sources and includes

--- setup.py
+++ setup.py
@@ -6,25 +6,33 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 this_dir = osp.dirname(osp.abspath(__file__))
 _ext_src_root = osp.join("pointops", "_ext-src")
-_ext_sources = glob.glob(osp.join(_ext_src_root, "src", "*.cpp")) + glob.glob(
-    osp.join(_ext_src_root, "src", "*.cu")
-)
+_ext_sources = glob.glob(
+    osp.join(_ext_src_root, "src", "**", "*.cpp"), recursive=True
+) + glob.glob(osp.join(_ext_src_root, "src", "**", "*.cu"), recursive=True)
+
 
 requirements = ["torch>=1.4"]
 
 setup(
     name="pointops",
     version="3.0.0",
+    install_requires=requirements,
     packages=find_packages(),
     ext_modules=[
         CUDAExtension(
-            name="fps_cuda._ext",
+            name="pointops._ext",
             sources=_ext_sources,
             extra_compile_args={
                 "cxx": ["-g"],
                 "nvcc": ["-O2"],
             },
-            include_dirs=[osp.join(this_dir, _ext_src_root, "include")],
+            include_dirs=[
+                d
+                for d in glob.glob(
+                    osp.join(this_dir, _ext_src_root, "include", "**"), recursive=True
+                )
+                if osp.isdir(d)
+            ],
         )
     ],
     cmdclass={"build_ext": BuildExtension},
